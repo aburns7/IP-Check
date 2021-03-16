042b55085f223a8beda8ac489273b913c02d92e5
adam burns <adam.burns@ibm.com>
2021-03-15 20:56:23 -0500

    adding a unit test and README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # IP-Check
+
+IP check is a simple python 3 script that gets json data and checks if a given IP is in the Ipv4 CIDR Ranges
+
+## Installation
+Clone the repo and then install the dependencies
+```bash
+pip install -r requirements.txt
+```
+## Usage
+
+```bash
+./ip_check <ip address>
+```
+
+### Output
+```bash
+$ python ip_check.py 192.168.004.001
+Fail: 192.168.4.1 is not found in the ipv4 CIDR ranges
+```
+```bash
+$ python ip_check.py 69.208.0.1
+Pass: 69.208.0.1 is found in the ipv4 CIDR range
+```
---
+++ test_ip_check.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import requests
+import sys
+import json 
+import pytest
+from netaddr import * 
+
+def test_find_ip():
+    #API call to JSON Data
+    ip = IPAddress('192.208.0.0')
+
+    ip_set = (['69.208.0.0/15','2.56.32.0/22','192.124.99.0/24'])
+    r = requests.get("https://stat.ripe.net/data/country-resource-list/data.json?resource=US&v4_format=prefix")
+    #Grab ipv4 address blocks into a set
+    ipv4_set = set(r.json()['data']['resources']['ipv4'])
+    count = 0
+    # Compare ip address with the set and add 1 to count for match
+    for x in ipv4_set:
+        if ip in IPNetwork(x): count+=1
+    if count >= 1: 
+        print(f"Pass: {ip} is found in the ipv4 CIDR ranges")
+    else:
+        print(f"Fail: {ip} is not found in the ipv4 CIDR ranges")
+    
